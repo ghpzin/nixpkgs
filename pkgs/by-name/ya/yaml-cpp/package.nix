6eb5faaf61cdf403f93304ef979976067d0551c4
ghpzin <ghpzin@gmail.com>
2025-09-16 15:36:43 +0300

    (PR-446120) yaml-cpp: fix build with gcc15

--- pkgs/by-name/ya/yaml-cpp/package.nix
+++ pkgs/by-name/ya/yaml-cpp/package.nix
@@ -25,6 +25,11 @@ stdenv.mkDerivation rec {
       url = "https://github.com/jbeder/yaml-cpp/commit/c2680200486572baf8221ba052ef50b58ecd816e.patch";
       hash = "sha256-1kXRa+xrAbLEhcJxNV1oGHPmayj1RNIe6dDWXZA3mUA=";
     })
+    (fetchpatch {
+      name = "yaml-cpp-fix-gcc15.patch";
+      url = "https://github.com/jbeder/yaml-cpp/commit/7b469b4220f96fb3d036cf68cd7bd30bd39e61d2.patch?full_index=1";
+      hash = "sha256-4Mua6cYD8UR+fJfFeu0fdYVFprsiuF89HvbaTByz9nI=";
+    })
   ];
 
   strictDeps = true;
