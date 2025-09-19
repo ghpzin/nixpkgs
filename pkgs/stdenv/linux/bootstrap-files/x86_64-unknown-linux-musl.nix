16b9f24f001c2f1b182a250c1281fdf7f0c65a9e
ghpzin <ghpzin@gmail.com>
2025-09-19 16:37:07 +0300

    pkgs/stdenv/linux: update x86_64-unknown-linux-musl bootstrap-files
    sha256sum of files to be uploaded:
    
    $ sha256sum /nix/store/l916l2hh3xn65x07j3h0p6qkxc9r3y9n-stdenv-bootstrap-tools/on-server/*
    46b42b80c26237bed86f00a4a5e362bed989cebcadd24268ff147e51db8b79d2  /nix/store/l916l2hh3xn65x07j3h0p6qkxc9r3y9n-stdenv-bootstrap-tools/on-server/bootstrap-tools.tar.xz
    9fdc1dd38b23d214d54d95e46b24bffd21d89193daad88aca5768ad81b638aac  /nix/store/l916l2hh3xn65x07j3h0p6qkxc9r3y9n-stdenv-bootstrap-tools/on-server/busybox
    
    Suggested commands to upload files to 'tarballs.nixos.org':
    
        $ nix-store --realize /nix/store/l916l2hh3xn65x07j3h0p6qkxc9r3y9n-stdenv-bootstrap-tools
        $ aws s3 cp --recursive --acl public-read /nix/store/l916l2hh3xn65x07j3h0p6qkxc9r3y9n-stdenv-bootstrap-tools/on-server/ s3://nixpkgs-tarballs/stdenv/x86_64-unknown-linux-musl/01b6809f7f9d1183a2b3e081f0a1e6f8f415cb09
        $ aws s3 cp --recursive s3://nixpkgs-tarballs/stdenv/x86_64-unknown-linux-musl/01b6809f7f9d1183a2b3e081f0a1e6f8f415cb09 ./
        $ sha256sum bootstrap-tools.tar.xz busybox
        $ sha256sum /nix/store/l916l2hh3xn65x07j3h0p6qkxc9r3y9n-stdenv-bootstrap-tools/on-server/*

--- pkgs/stdenv/linux/bootstrap-files/x86_64-unknown-linux-musl.nix
+++ pkgs/stdenv/linux/bootstrap-files/x86_64-unknown-linux-musl.nix
@@ -2,20 +2,20 @@
 # $ ./refresh-tarballs.bash --targets=x86_64-unknown-linux-musl
 #
 # Metadata:
-# - nixpkgs revision: 125cefd4cf8f857e5ff1aceaef9230ba578a033d
+# - nixpkgs revision: 01b6809f7f9d1183a2b3e081f0a1e6f8f415cb09
 # - hydra build: https://hydra.nixos.org/job/nixpkgs/trunk/stdenvBootstrapTools.x86_64-unknown-linux-musl.build/latest
-# - resolved hydra build: https://hydra.nixos.org/build/247890807
-# - instantiated derivation: /nix/store/gqri9n85rsf2983r6m8lkz0h69k4n7xi-stdenv-bootstrap-tools.drv
-# - output directory: /nix/store/b0x0qcbf1gsp50jzw52sbbgdp3jlwcjf-stdenv-bootstrap-tools
-# - build time: Fri, 26 Jan 2024 22:09:22 +0000
+# - resolved hydra build: https://hydra.nixos.org/build/310745962
+# - instantiated derivation: /nix/store/v2j8fy2az0k2q7w7jjy1jcm3fn1p704x-stdenv-bootstrap-tools.drv
+# - output directory: /nix/store/l916l2hh3xn65x07j3h0p6qkxc9r3y9n-stdenv-bootstrap-tools
+# - build time: Wed, 22 Oct 2025 21:42:38 +0000
 {
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = "http://tarballs.nixos.org/stdenv/x86_64-unknown-linux-musl/125cefd4cf8f857e5ff1aceaef9230ba578a033d/bootstrap-tools.tar.xz";
-    hash = "sha256-t0W2MR7UwtPyYEGcRo9UOuXfaP4uUZKZXEmYGcBOuOA=";
+    url = "http://tarballs.nixos.org/stdenv/x86_64-unknown-linux-musl/01b6809f7f9d1183a2b3e081f0a1e6f8f415cb09/bootstrap-tools.tar.xz";
+    hash = "sha256-RrQrgMJiN77YbwCkpeNivtmJzryt0kJo/xR+UduLedI=";
   };
   busybox = import <nix/fetchurl.nix> {
-    url = "http://tarballs.nixos.org/stdenv/x86_64-unknown-linux-musl/125cefd4cf8f857e5ff1aceaef9230ba578a033d/busybox";
-    hash = "sha256-0U2r3EU61oqhs+oyzFABIFTCVqXOWSP0qEtnyHwjzm0=";
+    url = "http://tarballs.nixos.org/stdenv/x86_64-unknown-linux-musl/01b6809f7f9d1183a2b3e081f0a1e6f8f415cb09/busybox";
+    hash = "sha256-4DD7VQNeKFeRpKQVJZ/PBtTGZpAMR/+od9/di++6ApY=";
     executable = true;
   };
 }
