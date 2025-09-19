51496d1cd2f9ecc8bbf3a78594e8fb7a58032165
ghpzin <ghpzin@gmail.com>
2025-09-19 08:48:35 +0300

    (PR-446154) libodfgen: fix build with gcc15

--- pkgs/by-name/li/libodfgen/package.nix
+++ pkgs/by-name/li/libodfgen/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   stdenv,
+  fetchpatch,
   fetchurl,
   boost,
   pkg-config,
@@ -19,6 +20,15 @@ stdenv.mkDerivation rec {
     url = "mirror://sourceforge/project/libwpd/libodfgen/libodfgen-${version}/libodfgen-${version}.tar.xz";
     sha256 = "sha256-Mj5JH5VsjKKrsSyZjjUGcJMKMjF7+WYrBhXdSzkiuDE=";
   };
+
+  patches = [
+    # Fix build with gcc15
+    (fetchpatch {
+      name = "libodfgen-fix-includes-gcc15.patch";
+      url = "https://src.fedoraproject.org/rpms/libodfgen/raw/728cf6e824b479902f844978e75e0c7550eae61b/f/includes.patch";
+      hash = "sha256-NamJMJ0KekgOI4cD9/bcq8UlG9polMmITZ628+p+2OA=";
+    })
+  ];
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [
