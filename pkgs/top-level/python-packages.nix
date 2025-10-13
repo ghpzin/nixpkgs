714c72d168509245136a8ddfb314512cbabf24bb
ghpzin <ghpzin@gmail.com>
2025-10-13 12:01:53 +0300

    python3Packages.numcodecs: 0.16.1 -> 0.16.3
    Diff: https://github.com/zarr-developers/numcodecs/compare/v0.16.1...v0.16.3
    
    Changelog:
    https://github.com/zarr-developers/numcodecs/releases/tag/v0.16.2
    https://github.com/zarr-developers/numcodecs/releases/tag/v0.16.3
    
    Contains merged PR fixing build with gcc15:
    https://www.github.com/zarr-developers/numcodecs/pull/772
    https://github.com/zarr-developers/numcodecs/commit/878d2b385e4f1c6b113aca765e8632354abb3c9c
    
    Fixes build failure with gcc15:
    ```
    c-blosc/blosc/shuffle.c:26:15: error: ‘bool’ cannot be defined via ‘typedef’
       26 | typedef _Bool bool;
          |               ^~~~
    c-blosc/blosc/shuffle.c:26:15: note: ‘bool’ is a keyword with ‘-std=c23’ onwards
    c-blosc/blosc/shuffle.c:26:1: warning: useless type name in empty declaration
       26 | typedef _Bool bool;
          | ^~~~~~~
    In file included from c-blosc/blosc/shuffle.c:12:
    c-blosc/blosc/shuffle-generic.h:61:13: warning: ‘unshuffle_generic_inline’
    defined but not used [-Wunused-function]
       61 | static void unshuffle_generic_inline(const size_t type_size,
          |             ^~~~~~~~~~~~~~~~~~~~~~~~
    c-blosc/blosc/shuffle-generic.h:32:13: warning: ‘shuffle_generic_inline’
    defined but not used [-Wunused-function]
       32 | static void shuffle_generic_inline(const size_t type_size,
          |             ^~~~~~~~~~~~~~~~~~~~~~
    ```

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10976,7 +10976,7 @@ self: super: with self; {
 
   numbagg = callPackage ../development/python-modules/numbagg { };
 
-  numcodecs = callPackage ../development/python-modules/numcodecs { };
+  numcodecs = callPackage ../development/python-modules/numcodecs { inherit (pkgs) zstd; };
 
   numdifftools = callPackage ../development/python-modules/numdifftools { };
 
