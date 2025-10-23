63e7a841972ebeedfb7582d3dbf57f7eb51cf1a4
ghpzin <ghpzin@gmail.com>
2025-10-23 10:59:10 +0300

    shapelib: fix build with gcc15
    - add patch from merged upstream PR:
    https://www.github.com/OSGeo/shapelib/pull/188
    https://github.com/OSGeo/shapelib/commit/bf455986557e88cce6a2a85448798e1dc05541f2
    
    Fixes build failure with gcc15:
    ```
    dbfadd.c: In function 'main':
    dbfadd.c:48:10: error: 'bool' cannot be used here
       48 |     char bool;
          |          ^~~~
    dbfadd.c:48:10: note: 'bool' is a keyword with '-std=c23' onwards
    dbfadd.c:48:5: warning: useless type name in empty declaration
       48 |     char bool;
          |     ^~~~
    dbfadd.c:67:49: error: expected expression before 'bool'
       67 |             if (1 == sscanf(argv[i + 2], "%c", &bool))
          |                                                 ^~~~
    dbfadd.c:69:60: error: expected expression before 'bool'
       69 |                 DBFWriteLogicalAttribute(hDBF, iRecord, i, bool);
          |                                                            ^~~~
    ```

--- pkgs/by-name/sh/shapelib/package.nix
+++ pkgs/by-name/sh/shapelib/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchurl,
+  fetchpatch,
 }:
 
 stdenv.mkDerivation rec {
@@ -12,6 +13,16 @@ stdenv.mkDerivation rec {
     url = "https://download.osgeo.org/shapelib/shapelib-${version}.tar.gz";
     hash = "sha256-S3SjbO2U6ae+pAEVfmZK3cxb4lHn33+I1GdDYdoBLCE=";
   };
+
+  patches = [
+    # Fix build with gcc15
+    # https://github.com/OSGeo/shapelib/pull/188
+    (fetchpatch {
+      name = "shapelib-fix-build-with-gcc15.patch";
+      url = "https://github.com/OSGeo/shapelib/commit/bf455986557e88cce6a2a85448798e1dc05541f2.patch";
+      hash = "sha256-ubd8L2hxSAxTDiOSToVHGLHkpGOap5bnozdVdv9VgCQ=";
+    })
+  ];
 
   doCheck = true;
   preCheck = ''
