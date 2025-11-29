a07a4f84a65e19c3b63ab320ff45b06b84419600
ghpzin <ghpzin@gmail.com>
2025-11-29 19:06:46 +0300

    (TODO) pkgsMusl.patchutils: fix build with gcc15
    https://www.github.com/twaugh/patchutils/issues/65
    
    https://www.github.com/twaugh/patchutils/pull/75
    https://github.com/twaugh/patchutils/commit/dc88e964427c3d4a376a21dd517ab95c7b6c6ad4

--- pkgs/tools/text/patchutils/default.nix
+++ pkgs/tools/text/patchutils/default.nix
@@ -1,9 +1,14 @@
-{ callPackage, ... }@args:
+{ callPackage, stdenv, lib, ... }@args:
 
 callPackage ./generic.nix (
   args
   // {
     version = "0.3.4";
     sha256 = "0xp8mcfyi5nmb5a2zi5ibmyshxkb1zv1dgmnyn413m7ahgdx8mfg";
+    patches = lib.optionals (stdenv.hostPlatform.isMusl) [
+      # Fix build on musl with gcc15
+      # https://github.com/twaugh/patchutils/commit/dc88e964427c3d4a376a21dd517ab95c7b6c6ad4
+      # ./patchutils-gnulib-integrate-properly.patch
+    ];
   }
 )
