f538221195eb3d97c2b97ee136dccfc477413030
ghpzin <ghpzin@gmail.com>
2025-12-14 21:36:55 +0300

    (TODO) rocksdb_{6_23,7_10,8_3,8_11,9_10}: fix build with gcc15
    - 
    
    Fixes build failure with gcc15:
    ```
    /build/source/include/rocksdb/sst_partitioner.h:30:22: error: 'uint64_t'
    has not been declared
       30 |                      uint64_t current_output_file_size_)
          |                      ^~~~~~~~
    /build/source/include/rocksdb/sst_partitioner.h:14:1: note: 'uint64_t'
    is defined in header '<cstdint>'; this is probably fixable by
    adding '#include <cstdint>'
       13 | #include "rocksdb/slice.h"
      +++ |+#include <cstdint>
       14 |
    /build/source/db/blob/blob_file_meta.h:28:7: error: 'uint64_t' has not
    been declared
       28 |       uint64_t blob_file_number, uint64_t total_blob_count,
          |       ^~~~~~~~
    /build/source/db/blob/blob_file_meta.h:15:1: note: 'uint64_t' is
    defined in header '<cstdint>'; this is probably fixable by
    adding '#include <cstdint>'
       14 | #include "rocksdb/rocksdb_namespace.h"
      +++ |+#include <cstdint>
       15 |
    /build/source/include/rocksdb/write_batch_base.h:59:47: error:
    'uint64_t' has not been declared
       59 |                           const Slice& value, uint64_t write_unix_time) = 0;
          |                                               ^~~~~~~~
    /build/source/include/rocksdb/write_batch_base.h:14:1: note: 'uint64_t'
    is defined in header '<cstdint>'; this is probably fixable by
    adding '#include <cstdint>'
       13 | #include "rocksdb/attribute_groups.h"
      +++ |+#include <cstdint>
       14 | #include "rocksdb/rocksdb_namespace.h"
    ```

--- pkgs/by-name/ro/rocksdb/package.nix
+++ pkgs/by-name/ro/rocksdb/package.nix
@@ -89,12 +89,17 @@ stdenv.mkDerivation (finalAttrs: {
       sed -e '1i #include <cstdint>' -i table/block_based/data_block_hash_index.h
       sed -e '1i #include <cstdint>' -i util/string_util.h
       sed -e '1i #include <cstdint>' -i include/rocksdb/utilities/checkpoint.h
+      sed -e '1i #include <cstdint>' -i include/rocksdb/sst_partitioner.h
     ''
     + lib.optionalString (lib.versionOlder finalAttrs.version "7") ''
       # Fix gcc-13 build failures due to missing <cstdint> and
       # <system_error> includes, fixed upstyream sice 7.x
       sed -e '1i #include <system_error>' -i third-party/folly/folly/synchronization/detail/ProxyLockable-inl.h
-    '';
+    ''
+    + lib.optionalString (lib.versionOlder finalAttrs.version "10") ''
+      sed -e '1i #include <cstdint>' -i db/blob/blob_file_meta.h
+    ''
+    ;
 
   preInstall = ''
     mkdir -p $tools/bin
