4ff3ab7d0ff2a96b78b2a423f61c693cf874ebfe
Gaetan Lepage <gaetan@glepage.com>
2026-02-21 20:12:23 +0000

    python3Packages.shellingham: cleanup

--- pkgs/development/python-modules/shellingham/default.nix
+++ pkgs/development/python-modules/shellingham/default.nix
@@ -7,7 +7,7 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "shellingham";
   version = "1.5.4";
   pyproject = true;
@@ -15,11 +15,11 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "sarugaku";
     repo = "shellingham";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-xeBo3Ok+XPrHN4nQd7M8/11leSV/8z1f7Sj33+HFVtQ=";
   };
 
-  nativeBuildInputs = [ setuptools ];
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [
     pytest-mock
@@ -31,8 +31,8 @@ buildPythonPackage rec {
   meta = {
     description = "Tool to detect the surrounding shell";
     homepage = "https://github.com/sarugaku/shellingham";
-    changelog = "https://github.com/sarugaku/shellingham/blob/${version}/CHANGELOG.rst";
+    changelog = "https://github.com/sarugaku/shellingham/blob/${finalAttrs.src.tag}/CHANGELOG.rst";
     license = lib.licenses.isc;
     maintainers = with lib.maintainers; [ mbode ];
   };
-}
+})
