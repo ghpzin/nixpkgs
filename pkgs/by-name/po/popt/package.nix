6c168ae5b5fbe319d189656681a65d0535208307
David Wronek <david.wronek@mainlining.org>
2026-02-21 12:49:27 +0100

    popt: hardcode sysconfdir to /etc
    While stracing a program that uses popt for parsing command line
    options, we can observe that it tries to look for the configuration file
    in /nix/store/../etc instead of /etc:
    
    openat(AT_FDCWD, "/nix/store/yywbzjm7yy89y5z23jazngbv4585rqbr-popt-1.19/etc/popt", O_RDONLY) = -1 ENOENT (No such file or directory)
    
    This is obviously wrong, as it would hinder a user from modifying the
    system-wide popt configuration (e.g. using environment.etc on a NixOS
    system). Let's fix this issue by explicitly setting --sysconfdir to
    /etc.
    
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- pkgs/by-name/po/popt/package.nix
+++ pkgs/by-name/po/popt/package.nix
@@ -41,6 +41,11 @@ stdenv.mkDerivation (finalAttrs: {
       })
     ];
 
+  configureFlags = [
+    # Otherwise, it configures sysconfdir as $out/etc.
+    "--sysconfdir=/etc"
+  ];
+
   doCheck = false; # fails
 
   meta = {
